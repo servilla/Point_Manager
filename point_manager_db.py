71d7b3fbd4a9c1e5e58108c74dc79764df6a5ae7
Mark Servilla <mark.servilla@gmail.com>
2016-08-28 21:03:55 -0600

    Modify sensors table schema to include date, datetime, and point manager attributes. Add db table creation/connection and add_sensor methods.

--- point_manager_db.py
+++ point_manager_db.py
@@ -12,9 +12,9 @@
     8/27/16
 """
 
-from datetime import datetime
-from sqlalchemy import Table, Column, Integer, Float, String, DateTime
+from sqlalchemy import Column, Integer, Float, String, Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -29,14 +29,16 @@ logger = logging.getLogger('point_manager_db')
 class Sensor(Base):
     #Sensor DB ORM
 
-    __tablename__ = 'sensor'
+    __tablename__ = 'sensors'
 
-    index = Column(Integer, primary_key=True)
+    index = Column(Integer, primary_key=True, autoincrement=True)
+    point_manager_id = Column(String, nullable=False)
+    point_manager_timestamp = Column(DateTime, nullable=False)
     sensor_id = Column(Integer, nullable=False)
     sensor_type = Column(Integer, nullable=False)
     sensor_serial_no = Column(String, nullable=False)
     sensor_battery = Column(Integer, nullable=False)
-    sensor_battery_life = Column(DateTime, nullable=False)
+    sensor_battery_life = Column(Date, nullable=False)
     sensor_age = Column(Integer, nullable=False)
     sensor_status = Column(Integer, nullable=False)
     sensor_service = Column(Integer, nullable=False)
@@ -51,18 +53,44 @@ class Sensor(Base):
 def get_sensor_db_cwd():
     import os
     cwd = os.getcwd()
-    return cwd + '/sensor.sqlite'
+    return cwd + '/sensors.db'
 
-def create_sqlite3_db():
+
+def connect_sensor_db():
     from sqlalchemy import create_engine
     sensor_db_path = get_sensor_db_cwd()
     engine = create_engine('sqlite:///' + sensor_db_path)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def add_sensor(pm_id=None, pm_ts=None, s=None, session=None):
+    sensor = Sensor(
+        point_manager_id=pm_id,
+        point_manager_timestamp=pm_ts,
+        sensor_id=s.get_id(),
+        sensor_type=s.get_type(),
+        sensor_serial_no=s.get_snid(),
+        sensor_battery=s.get_batt(),
+        sensor_battery_life=s.get_battlife(),
+        sensor_age=s.get_Age(),
+        sensor_status=s.get_Status(),
+        sensor_service=s.get_Service(),
+        point_id=s.get_Point().get_id(),
+        point_type=s.get_Point().get_type(),
+        point_data_type=s.get_Point().get_dtype(),
+        point_serial_no=s.get_Point().get_ptid(),
+        point_index=s.get_Point().get_index(),
+        point_value=s.get_Point().get_Value_as_F()
+    )
+    session.add(sensor)
+    session.commit()
 
 
 def main():
 
-    create_sqlite3_db()
+    connect_sensor_db()
 
     return 0
 
