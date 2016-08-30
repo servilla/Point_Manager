b2c00a9bddaff70680c648aed9629b4d43d736c5
Mark Servilla <mark.servilla@gmail.com>
2016-08-29 21:19:49 -0600

    Add SensorCooked object to represent modified data; change Sensor to SensorRaw to represent raw data.

--- point_manager_db.py
+++ point_manager_db.py
@@ -15,6 +15,7 @@
 from sqlalchemy import Column, Integer, Float, String, Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime, timedelta
 
 Base = declarative_base()
 
@@ -26,14 +27,40 @@ logging.getLogger('').setLevel(logging.WARN)
 logger = logging.getLogger('point_manager_db')
 
 
-class Sensor(Base):
+class SensorRaw(Base):
     #Sensor DB ORM
 
-    __tablename__ = 'sensors'
+    __tablename__ = 'sensors_raw'
 
     index = Column(Integer, primary_key=True, autoincrement=True)
+    collection_timestamp = Column(DateTime, nullable=False)
     point_manager_id = Column(String, nullable=False)
-    point_manager_timestamp = Column(DateTime, nullable=False)
+    point_manager_ts = Column(String, nullable=False)
+    sensor_id = Column(Integer, nullable=False)
+    sensor_type = Column(Integer, nullable=False)
+    sensor_serial_no = Column(String, nullable=False)
+    sensor_battery = Column(Integer, nullable=False)
+    sensor_battery_life = Column(String, nullable=False)
+    sensor_age = Column(Integer, nullable=False)
+    sensor_status = Column(Integer, nullable=False)
+    sensor_service = Column(Integer, nullable=False)
+    point_id = Column(Integer, nullable=False)
+    point_type = Column(String, nullable=False)
+    point_data_type = Column(Integer, nullable=False)
+    point_serial_no = Column(String, nullable=False)
+    point_index = Column(Integer, nullable=False)
+    point_value = Column(Float, nullable=False)
+
+
+class SensorCooked(Base):
+    #Sensor DB ORM
+
+    __tablename__ = 'sensors_cooked'
+
+    index = Column(Integer, primary_key=True, autoincrement=True)
+    collection_timestamp = Column(DateTime, nullable=False)
+    point_manager_id = Column(String, nullable=False)
+    sensor_timestamp = Column(DateTime, nullable=False)
     sensor_id = Column(Integer, nullable=False)
     sensor_type = Column(Integer, nullable=False)
     sensor_serial_no = Column(String, nullable=False)
@@ -65,26 +92,51 @@ def connect_sensor_db():
     return Session()
 
 
-def add_sensor(pm_id=None, pm_ts=None, s=None, session=None):
-    sensor = Sensor(
+def add_sensor_raw(ts=None, pm_id=None, pm_ts=None, sensor=None, session=None):
+    s = SensorRaw(
+        collection_timestamp=ts,
         point_manager_id=pm_id,
-        point_manager_timestamp=pm_ts,
-        sensor_id=s.get_id(),
-        sensor_type=s.get_type(),
-        sensor_serial_no=s.get_snid(),
-        sensor_battery=s.get_batt(),
-        sensor_battery_life=s.get_battlife(),
-        sensor_age=s.get_Age(),
-        sensor_status=s.get_Status(),
-        sensor_service=s.get_Service(),
-        point_id=s.get_Point().get_id(),
-        point_type=s.get_Point().get_type(),
-        point_data_type=s.get_Point().get_dtype(),
-        point_serial_no=s.get_Point().get_ptid(),
-        point_index=s.get_Point().get_index(),
-        point_value=s.get_Point().get_Value_as_F()
+        point_manager_ts=pm_ts,
+        sensor_id=sensor.get_id(),
+        sensor_type=sensor.get_type(),
+        sensor_serial_no=sensor.get_snid(),
+        sensor_battery=sensor.get_batt(),
+        sensor_battery_life=sensor.get_battlife(),
+        sensor_age=sensor.get_Age(),
+        sensor_status=sensor.get_Status(),
+        sensor_service=sensor.get_Service(),
+        point_id=sensor.get_Point().get_id(),
+        point_type=sensor.get_Point().get_type(),
+        point_data_type=sensor.get_Point().get_dtype(),
+        point_serial_no=sensor.get_Point().get_ptid(),
+        point_index=sensor.get_Point().get_index(),
+        point_value=sensor.get_Point().get_Value()
     )
-    session.add(sensor)
+    session.add(s)
+    session.commit()
+
+
+def add_sensor_cooked(ts=None, pm_id=None, sensor=None, session=None):
+    s = SensorCooked(
+        collection_timestamp=ts,
+        point_manager_id=pm_id,
+        sensor_timestamp=(ts - timedelta(seconds=sensor.get_Age())),
+        sensor_id=sensor.get_id(),
+        sensor_type=sensor.get_type(),
+        sensor_serial_no=sensor.get_snid(),
+        sensor_battery=sensor.get_batt(),
+        sensor_battery_life=sensor.get_battlife(as_date=True),
+        sensor_age=sensor.get_Age(),
+        sensor_status=sensor.get_Status(),
+        sensor_service=sensor.get_Service(),
+        point_id=sensor.get_Point().get_id(),
+        point_type=sensor.get_Point().get_type(),
+        point_data_type=sensor.get_Point().get_dtype(),
+        point_serial_no=sensor.get_Point().get_ptid(),
+        point_index=sensor.get_Point().get_index(),
+        point_value=sensor.get_Point().get_Value(as_celsius=True)
+    )
+    session.add(s)
     session.commit()
 
 
